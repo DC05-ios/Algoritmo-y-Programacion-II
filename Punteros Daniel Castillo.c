17a8b573557ee469cc23777f5b757fe66436047b
Daniel Castillo <danialecasrob123@gmail.com>
2025-02-22 13:58:00 -0400

    Update Punteros Daniel Castillo.c
    Comentarios de explicacion del puntero

--- Punteros Daniel Castillo.c
+++ Punteros Daniel Castillo.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> // Aplicamos las bibliotecas
 
-void intercambiar(int *a, int *b)
+void intercambiar(int *a, int *b) // Utilizamos void para declarar punteros que pueden apuntar a cualquier tipo de dato 
 {
     int temp = *a;
     *a = *b;
-    *b = temp;
+    *b = temp; // Identificamos las variables
 
 }
 
-int main()
+int main() // De aqui implicitamente inicia el programa
 {
 
-    int x = 9000, y = 50000;
-    printf("Antes de realizar el intercambio: x = %d, y = %d\n", x, y);
+    int x = 9000, y = 50000; // Damos valor a las variables
+    printf("Antes de realizar el intercambio: x = %d, y = %d\n", x, y); // Aqui con printf, muestra en la ejecución del programa el enunciado indicado
 
-    intercambiar(&x, &y);
+    intercambiar(&x, &y); // Aqui con & guarda la informacion de las variables y despues realiza el "Intercambio"
 
-    printf("Despues de realizar el intercambio: x = %d, y = %d\n", x, y);
+    printf("Despues de realizar el intercambio: x = %d, y = %d\n", x, y); // Ya aqui muestra el enunciado
 
     return 0;
 }
